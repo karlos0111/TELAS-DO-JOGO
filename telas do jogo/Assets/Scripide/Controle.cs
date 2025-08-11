549fd4df3553ee0fe23a98b0827cf684cfdd3128
karlos0111 <karlos.lima@escolar.ifrn.edu.br>
2025-08-11 16:19:39 -0300

    volme

---
+++ telas do jogo/Assets/Scripide/Controle.cs
@@ -0,0 +1,43 @@
+using System;
+using NUnit.Framework.Internal;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controle : MonoBehaviour
+{
+    public int volume;
+    public int volumeSFX;
+    public bool Musica;
+    public TextMeshPro textomusica;
+        
+        
+    public Slider volumeSlider;
+    public Slider volumeSFXSlider;
+    public Toggle togglemusica;
+
+    void Start()
+    {
+        Musica=togglemusica.isOn;
+        volume=(int) volumeSlider.value;
+        volumeSFX =(int) volumeSFXSlider.value;
+    }
+
+     void Update()
+    {
+        Musica=togglemusica.isOn;
+        volume=(int) volumeSlider.value;
+        volumeSFX = (int)volumeSFXSlider.value;
+
+        if (Musica == true)
+        {
+            textomusica.text = "ligado";
+            textomusica.color = Color.green;
+        }
+        else
+        {
+            textomusica.text = "Desligado";
+            textomusica.color = Color.red;
+        }
+    }
+}
